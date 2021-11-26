3fc50c74d1cfcd2e9e70b42ce78c8184cdd28364
tangid <tangi.delanoe@hotmail.fr>
2021-11-26 13:37:56 +0100

    UserCase4 part1

--- main.py
+++ main.py
@@ -1,50 +1,23 @@
 from tkinter import *
 import tkinter
 from Todo import Todo
-
+import function
 #Initialisation Todo
-todoList = [Todo('Make user case'), Todo('Make the next one'), Todo('Get so bread')]
+function.todoList = [Todo('Make user case'), Todo('Make the next one'), Todo('Get so bread')]
 
-
-"""
-def edit():
-    for i in range(len(todoList)):
-        if listButtonValue[i].get():
-            todoList[i].state = 1
-        else:
-            todoList[i].state = 0
-
-
-    #Update position
-    for i in range(len(todoList)):
-        if todoList[i].state:
-
-            #Update liste todo            
-            for i in range(len(todoList)):
-                if (todoList[i].state):
-                    todoList.append(todoList.pop(i))
-                liste.delete(i)
-            for i in range (len(todoList)):
-                pass
-                liste.insert(i, todoList[i].title)
-
-            #Update état bouton
-            if (todoList[i].state):
-                listButton[i].select()
-            else:
-                listButton[i].deselect()
-"""
+#Sauvegarde dans un fichier
 def save():
     fichier = open("todo.txt", "w")
-    for i in todoList:
+    for i in function.todoList:
         
         fichier.write(i.title + "|" + str(i.state) + "|" + i.description + '\n')
     fichier.close()
 
+#Remplace les todo par ceux dans le fichier
 def load():
-    for i in range(len(todoList)):
+    for i in range(len(function.todoList)):
         liste.delete(0)
-        todoList.pop(0)
+        function.todoList.pop(0)
 
     fichier = open("todo.txt", "r")
     ligne = "0|0"
@@ -54,20 +27,25 @@ def load():
         ligne = fichier.readline()
         if (len(ligne.split("|"))) > 1:
             title, state, description = ligne.split("|")
-            todoList.append(Todo(title, int(state)))
+            function.todoList.append(Todo(title, int(state)))
             liste.insert(i, title)
             i += 1
             print(i)
         else:
             break
 
+#Ajoute un todo dans la liste
+def add():
+    #Recuperer le titre
+    
+    function.todoList = [Todo("title")] + function.todoList
+    liste.insert(0, "title")
 
 
 
-
-todoList[1].state = 1
-print(todoList[1].state)
-for i in todoList:
+function.todoList[1].state = 1
+print(function.todoList[1].state)
+for i in function.todoList:
     i.print()
 
 #Initialisation Fenetre
@@ -79,38 +57,23 @@ fenetre.geometry("400x200")
 Frame1 = Frame(fenetre, borderwidth=2, relief=GROOVE)
 Frame1.pack(side=LEFT, padx=50, pady=30)
 
-"""
-# frame 2
-Frame2 = Frame(fenetre, borderwidth=2, relief=GROOVE)
-Frame2.pack(side=LEFT, padx=10, pady=10)
-"""
+
 #Implication des todos dans la fenêtre
 liste = Listbox(Frame1)
-for i in range (len(todoList)):
+for i in range (len(function.todoList)):
     #Met les todo fini à la fin
-    if (todoList[i].state == 1):
-        todoList.append(todoList.pop(i))
-    liste.insert(i, todoList[i].title)
+    if (function.todoList[i].state == 1):
+        function.todoList.append(function.todoList.pop(i))
+    liste.insert(i, function.todoList[i].title)
 liste.pack()
-"""
-#Creation bouton State
-listButton = []
-listButtonValue = []
-for i in range (len(todoList)):
-    listButtonValue.append(IntVar())
-    listButton.append(Checkbutton(Frame2, variable=listButtonValue[i], onvalue=1, offvalue=0))
-    #Check the box if state is done
-    if todoList[i].state == 1:
-        listButton[i].select()
-    listButton[i].pack()
-"""
+
 
 
 #Bouton mise à jour valeur state
 #Button(fenetre, text ='Edit state', command=edit).pack(side=RIGHT, padx=5, pady=5)
-Button(fenetre, text ='Save', command=save).pack(side=RIGHT, padx=5, pady=2)
+Button(fenetre, text ='Save', command=save).pack(side=RIGHT, padx=5, pady=10)
 Button(fenetre, text ='Load', command=load).pack(side=RIGHT, padx=5, pady=10)
-#Button(fenetre, text ='Add', command=add).pack(side=RIGHT, padx=5, pady=10)
+Button(fenetre, text ='Add', command=add).pack(side=RIGHT, padx=5, pady=10)
 
 Label(Frame1).pack(padx=10, pady=10)
 #Label(Frame2).pack(padx=10, pady=10)
